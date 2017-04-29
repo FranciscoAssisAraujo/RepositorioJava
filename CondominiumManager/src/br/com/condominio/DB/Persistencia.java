ca13b742010daff37e78b164dc33d938435a7c8c
FranciscoAssisAraujo <franciscoaraujo463@gmail.com>
2017-04-29 13:20:30 -0300

    Persistencia

--- CondominiumManager/src/br/com/condominio/DB/Persistencia.java
+++ CondominiumManager/src/br/com/condominio/DB/Persistencia.java
@@ -1,8 +1,13 @@
 package br.com.condominio.DB;
 
+import java.util.ArrayList;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.mapping.List;
+
+import br.com.condominio.condominioPO.CondominioPO;
 
 public class Persistencia {
 
@@ -57,6 +62,18 @@ public class Persistencia {
 		 return session.load(obj.getClass(), id);		
 	}
 	
+//	public ArrayList<CondominioPO> GetListCondominio(){
+//		return (List) session.createQuery("select * from tb_condominiopo").list();
+//	}
 
+	
+	 public ArrayList<CondominioPO> getlistaCondominios() {     
+	        ArrayList<CondominioPO> listaCondominios = new ArrayList<>(session.createCriteria(CondominioPO.class).list());
+	        for(CondominioPO p : listaCondominios){
+	            session.refresh(p);
+	        }
+	        return listaCondominios;
+	   }
+	
 
 }
